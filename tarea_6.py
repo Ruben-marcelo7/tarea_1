5c5c69c9a7baf167716977f8e755af3044ee29b5
Rubén <ruben.cobos@tecnologicoliceocristiano.edu.ec>
2024-07-16 17:07:46 -0500

    actvidad en clase

--- tarea_6.py
+++ tarea_6.py
@@ -2,8 +2,8 @@ def is_eligible_purchase(age, items_purchased):
 
   return age > 18 and items_purchased > 1
 
-age = int(input("tu edad: "))
-items_purchased = int(input("cosas compradas: "))
+edad = int(input("tu edad: "))
+CosasCompradas = int(input("cosas compradas: "))
 
-eligible = is_eligible_purchase(age, items_purchased)
-print(eligible)
+Resultado = is_eligible_purchase(edad, CosasCompradas)
+print(Resultado)
